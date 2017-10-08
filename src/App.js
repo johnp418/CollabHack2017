c46631b4f4fb9bf084370be96096e2088927605e
Minwoo Kim <mwkim0919@gmail.com>
2017-10-07 17:22:47 -0700

    front-end started.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,86 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+
+import Board from './components/board/Board'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark fixed-top">
+      <a className="navbar-brand" href="/">HOME</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="/board">Board</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/board2">Board2</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+const Home = () => {
+  return (
+    <div>
+      <div className="jumbotron">
+        <div className="container">
+          <h1 className="display-3">Hello, world!</h1>
+          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+          <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+          <div className="col-md-4">
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+          <div className="col-md-4">
+            <h2>Heading</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+        </div>
+
+        <hr/>
+
+        <footer>
+          <p>© Company 2017</p>
+        </footer>
+      </div>
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar />
+        <div className="container-fluid">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/board" component={Board} />
+        </div>
       </div>
     );
   }
