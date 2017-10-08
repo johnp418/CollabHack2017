9517394ae6521f43495e2999e20ac4ffe267165f
JohnPark <jongmanpark418@gmail.com>
2017-10-07 19:45:55 -0700

    ejected cra

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>, 
-	document.getElementById('root'));
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
