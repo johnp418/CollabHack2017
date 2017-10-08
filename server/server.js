d03d11a05a5862065049e6d630098a7e5393ca1f
JohnPark <jongmanpark418@gmail.com>
2017-10-07 19:07:01 -0700

    oauth2 wip...

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 const Express = require('express');
-// const session = require('express-session');
 const path = require('path');
+const cookieSession = require('cookie-session');
 const authApp = require('./authentication');
+
 const app = new Express();
 const port = 5000;
 
 // Default settings
 app.use(Express.json());
-app.use(Express.urlencoded({ extended: false }));
+app.use(Express.urlencoded({ extended: true }));
+app.use(cookieSession({ maxAge: 1000 * 100000, keys: ['hi'] }));
 
 // Register passport
 authApp(app);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(process.cwd(), 'public/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(process.cwd(), 'public/index.html'));
+// });
 
 app.listen(port);
