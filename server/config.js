a4cc4f6c1d5a1a0575d31754a365240cc4e51dab
JohnPark <jongmanpark418@gmail.com>
2017-10-07 17:27:48 -0700

    added login form, still working on authentication module

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  google: {
+    clientID:
+      '421637692305-fhee4igu09e5a3coso83pua38m4bs4br.apps.googleusercontent.com',
+    clientSecret: 'EQk8009VT-v0IvMSkwhZ4N1P'
+  }
+};
--- src/App.js
+++ src/App.js
@@ -7,31 +7,46 @@ import {
   Link,
   Redirect,
   withRouter
-} from "react-router-dom";
+} from 'react-router-dom';
 
-import Board from './components/board/Board'
+import Board from './components/board/Board';
+import Login from './components/login/Login';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark fixed-top">
-      <a className="navbar-brand" href="/">HOME</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+      <a className="navbar-brand" href="/">
+        HOME
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <a className="nav-link" href="/board">Board</a>
+            <a className="nav-link" href="/board">
+              Board
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/board2">Board2</a>
+            <a className="nav-link" href="/board2">
+              Board2
+            </a>
           </li>
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 const Home = () => {
   return (
@@ -39,30 +54,66 @@ const Home = () => {
       <div className="jumbotron">
         <div className="container">
           <h1 className="display-3">Hello, world!</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-          <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+          <p>
+            This is a template for a simple marketing or informational website.
+            It includes a large callout called a jumbotron and three supporting
+            pieces of content. Use it as a starting point to create something
+            more unique.
+          </p>
+          <p>
+            <a className="btn btn-primary btn-lg" href="#" role="button">
+              Learn more »
+            </a>
+          </p>
         </div>
       </div>
       <div className="container">
         <div className="row">
           <div className="col-md-4">
             <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+            <p>
+              Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+              tellus ac cursus commodo, tortor mauris condimentum nibh, ut
+              fermentum massa justo sit amet risus. Etiam porta sem malesuada
+              magna mollis euismod. Donec sed odio dui.{' '}
+            </p>
+            <p>
+              <a className="btn btn-secondary" href="#" role="button">
+                View details »
+              </a>
+            </p>
           </div>
           <div className="col-md-4">
             <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+            <p>
+              Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+              tellus ac cursus commodo, tortor mauris condimentum nibh, ut
+              fermentum massa justo sit amet risus. Etiam porta sem malesuada
+              magna mollis euismod. Donec sed odio dui.{' '}
+            </p>
+            <p>
+              <a className="btn btn-secondary" href="#" role="button">
+                View details »
+              </a>
+            </p>
           </div>
           <div className="col-md-4">
             <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+            <p>
+              Donec sed odio dui. Cras justo odio, dapibus ac facilisis in,
+              egestas eget quam. Vestibulum id ligula porta felis euismod
+              semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris
+              condimentum nibh, ut fermentum massa justo sit amet risus.
+            </p>
+            <p>
+              <a className="btn btn-secondary" href="#" role="button">
+                View details »
+              </a>
+            </p>
           </div>
         </div>
 
-        <hr/>
+        <hr />
 
         <footer>
           <p>© Company 2017</p>
@@ -70,7 +121,7 @@ const Home = () => {
       </div>
     </div>
   );
-}
+};
 
 class App extends Component {
   render() {
@@ -80,6 +131,7 @@ class App extends Component {
         <div className="container-fluid">
           <Route exact path="/" component={Home} />
           <Route exact path="/board" component={Board} />
+          <Route path="/login" component={Login} />
         </div>
       </div>
     );
