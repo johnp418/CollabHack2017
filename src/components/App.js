7d6289df5f01bddacf7ee95e3eecf9c67125f67c
bryanlee89 <minjae8989@gmail.com>
2017-10-09 20:44:27 -0700

    react-bootstrap added. Navbar is added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 class App extends Component {
   render () {
     return (
-      <div>
-        Hi this is react side
+      <div className="container">
+        <div>
+          <Header />
+        </div>
       </div>
     )
   }
